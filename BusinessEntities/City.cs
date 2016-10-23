03229bcefbae6df430286e670abb8fb922aaca96
Luka Cvetkovic <cvetko911@hotmail.com>
2016-10-23 20:48:56 +0200

    Model Grada

---
+++ BusinessEntities/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class City
+    {
+        public String Name { get; set; }
+        public String County { get; set; }
+
+        public CityInformation Information { get; set; }
+    }
+}
