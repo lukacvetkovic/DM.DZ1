e54627ffabecadca58ac8e0cfee980b7ab49d8a1
Luka Cvetkovic <cvetko911@hotmail.com>
2016-10-23 09:44:27 +0200

    Add project files.

---
+++ DM.DZ1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DM.DZ1.MvcApplication" Language="C#" %>
---
+++ DM.DZ1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DM.DZ1.Startup))]
+namespace DM.DZ1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
